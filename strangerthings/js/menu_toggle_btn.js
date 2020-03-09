d9f2154df70ff3dfe0bcf5a77c04a1561f172f16
sua-no <sua953@naver.com>
2020-03-09 18:12:17 +0900

    rightnav완료,toggle작업중 0309

---
+++ strangerthings/js/menu_toggle_btn.js
@@ -0,0 +1,5 @@
+let menuToggleBtn = document.getElementsByClassName("menuToggleBtn");
+menuToggleBtn[0].addEventListener("click", function() {
+    let gnb = document.getElementById("gnb");
+    gnb.style.display = "block";
+});
