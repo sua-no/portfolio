70b2992e635fc06ac1270a0d489e50542d43ef5a
sua-no <sua953@naver.com>
2020-05-25 18:14:42 +0900

    svg완료 popstate완료

--- js/main.js
+++ js/main.js
@@ -1,40 +1,33 @@
 window.addEventListener('DOMContentLoaded', function () {
     let wheelCheck = true;
 
-    // $('.menuBtn').on('mouseenter', menuHover);
-    // $('.menuBtn').on('mouseleave', menuLeave);
-    // window.addEventListener('popstate', function () {
-    //     pageMove(history.state.page);
-    // })
+    window.addEventListener('popstate', function () {
+        pageMove(history.state.page);
+    });
     $('.menuBtn').on('click', menuIn); //메뉴버튼 클릭시 메뉴 슬라이드
     $(window).on('wheel', menuSlide); //마우스 휠 시 메뉴화면으로 슬라이드
 
-    $('.menuList').on('click', pageMove);
-    function pageMove(clickList) {
-        // console.log($(this).data('page'));
-        // $('.page').each(function(){
-        //     if(clickList == $('.page').data('contents')){
+    $('.menuList').on('click', pageMove); //메뉴 클릭시 서브 페이지로 이동
 
-        //     }
-        // });
-
+    function pageMove(clickPage) {
+        if (event.target != window) { // 탭 클릭시 history.push, 내용변경
+            clickPage = $(this).data('page');
+            historyManager(clickPage);
+        }
+        console.log(history.state);
+        changePage(clickPage);
     }
-
-    function menuHover() {
-        console.log("asdasd");
-        $(this).parent().css({
-            width: 130
+    function changePage(clickPage) {
+        $('.page').each(function (i) {
+            if (clickPage == $('.page').eq(i).data('contents')) {
+                $('.page').eq(i).addClass('contents').siblings().removeClass('contents');
+            }
         });
     }
-    function menuLeave() {
-        $(this).parent().css({
-            width: 55
-        });
+    function historyManager(idx) {
+        history.pushState({ page: idx }, 'title', '?' + idx);
     }
     function menuIn() {
-        // $(this).parent().css({
-        //     width: '80vw'
-        // });
         $('.headerInner').toggleClass('active');
     }
     //메인 스크롤 시 메뉴화면으로 슬라이드
