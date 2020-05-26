1f3a3055bdbb973b2b8fc032fa6ce7e2de3c104e
sua-no <sua953@naver.com>
2020-05-26 20:03:03 +0900

    컬러박스 애니메이션 js

--- js/main.js
+++ js/main.js
@@ -1,47 +1,92 @@
 window.addEventListener('DOMContentLoaded', function () {
     let wheelCheck = true;
 
+
     window.addEventListener('popstate', function () {
-        pageMove(history.state.page);
+        pageChange(history.state.page);
     });
     $('.menuBtn').on('click', menuIn); //메뉴버튼 클릭시 메뉴 슬라이드
-    $(window).on('wheel', menuSlide); //마우스 휠 시 메뉴화면으로 슬라이드
+    $(window).on('wheel', menuSlide); //메인 스크롤 시 메뉴화면으로 슬라이드
 
-    $('.menuList').on('click', pageMove); //메뉴 클릭시 서브 페이지로 이동
+    $('.menuList').on('click', pageChange); //메뉴 클릭시 서브 페이지로 전환
 
-    function pageMove(clickPage) {
-        if (event.target != window) { // 탭 클릭시 history.push, 내용변경
+
+    function pageChange(clickPage) {
+        if (event.target != window) { // 뒤로가기 클릭시 이전 페이지로 전환
             clickPage = $(this).data('page');
             historyManager(clickPage);
+            $('.headerInner').removeClass('active');
         }
-        console.log(history.state);
-        changePage(clickPage);
+        pageManager(clickPage); //클릭한 페이지로  전환
+        // colorAni();
+        colorManger(clickPage);
     }
-    function changePage(clickPage) {
+    function pageManager(clickPage) {
         $('.page').each(function (i) {
             if (clickPage == $('.page').eq(i).data('contents')) {
                 $('.page').eq(i).addClass('contents').siblings().removeClass('contents');
+                $('.pageColor').addClass('colorZindex');
+                scrollYAdd(clickPage); //about 또는 work 페이지로 이동시 스크롤 생성
+            } else if (clickPage === '') { //main페이지로 이동시 서브페이지 정렬
+                $('.page').eq(i).siblings().removeClass('contents');
+                $('.pageColor').removeClass('colorZindex');
             }
         });
     }
-    function historyManager(idx) {
-        history.pushState({ page: idx }, 'title', '?' + idx);
+    function colorManger(clickPage) {
+
+
+        switch (clickPage) {
+            case 'about':
+                $('.pageColor').animate({
+                    width: '0%'
+                }, 3000, function () {
+                    console.log('dd');
+                    $('.pageColor').animate({
+                        width: '80%',
+                        height: '100%',
+                        right: '0%',
+                        background: '#febf00'
+                    }, 1000);
+                });
+                break;
+            case 'work':
+                break;
+            case 'contact':
+                break;
+            default: break;
+        }
+    }
+    function colorAni() {
+        $('.pageColor').animate({
+            width: 0
+            // height: 0
+        }, 600);
+    }
+    function historyManager(clickPage) { //url에 페이지 위치 알림
+        history.pushState({ page: clickPage }, 'title', '?' + clickPage);
+    }
+    function scrollYAdd(clickPage) {
+        $('.pageInner').scrollTop(0);
+        if (clickPage === 'about' || clickPage === 'work') {
+            $('.pageInner').addClass('overflowY');
+        } else { //스크롤 없는 페이지로 이동시 스크롤 제거
+            $('.pageInner').removeClass('overflowY');
+        }
     }
     function menuIn() {
         $('.headerInner').toggleClass('active');
     }
-    //메인 스크롤 시 메뉴화면으로 슬라이드
     function menuSlide(e) {
         let wheelDelta = e.originalEvent.deltaY;
-        if (wheelDelta > 0 && wheelCheck) { //스크롤 시 이벤트 1번 실행
+        if (wheelDelta > 0 && wheelCheck) { // wheelCheck : boolean값으로 스크롤 시 이벤트 1번 실행하게함
             wheelCheck = false;
             $('.pageInner').addClass('active');
             $('.scrollDown').addClass('active');
         } else if (wheelDelta < 0 && !wheelCheck) {
-            wheelCheck = true;
+            wheelCheck = true; //wheel 방향 변경시 wheelCheck값 변경
             $('.pageInner').removeClass('active');
             $('.scrollDown').removeClass('active');
         }
     }
-
 });
